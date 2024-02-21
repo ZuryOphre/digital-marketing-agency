1c46ae9c871ae2566960911d8fce70603a1d7f49
Zury Martinez <zury.martinez.saldivia@gmail.com>
2024-02-21 19:25:51 -0300

    About Blog and Contact page

---
+++ src/pages/about.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../app/globals.css';
+import LayoutCard from '@/components/LayoutAbout';
+
+
+const About = () => {
+    const images = ['uRKbhTFDchdNvcNyGapB']; 
+
+    return (
+        <div>
+            <LayoutCard>
+            </LayoutCard>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/pages/blog.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LayoutBlog from '@/components/LayoutBlog';
+import '../app/globals.css';
+
+const Blog = () => {
+    return (
+        <div>
+            <LayoutBlog />
+        </div>
+    );
+};
+
+export default Blog;
