985cfdf1549870ba6efe667b52f403e10eda094a
Zury Martinez <zury.martinez.saldivia@gmail.com>
2024-02-21 18:45:36 -0300

    Website Logo Banner
    This component renders the website's logo image using the next/image component. It imports the image located at /logo.png and sets its width and height to 300 pixels.

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Image from "next/image";
+
+const Banner = () => {
+    return <Image src="/logo.png" alt="logo" width={300} height={300} />;
+}
+
+export default Banner;
