f8807ab327d25164f6c2a1d6187cb1b4c642998b
Zury Martinez <zury.martinez.saldivia@gmail.com>
2024-02-23 00:04:05 -0300

    change logo and fix route

--- src/pages/ihome.jsx
+++ src/pages/ihome.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Banner from '../components/Banner';
+import Banner from '@/components/Banner';
 import FloatingSocialButtons from '@/components/FloatingSocialButtom';
 
 const Ihome = () => {
